3b53b6472dca29bff037138e7711553b2d157efb
Zechariah Meunier <72064608+zechmeunier@users.noreply.github.com>
2024-11-06 15:51:33 -0800

    Add time series plotting functions

--- src/Plotting.R
+++ src/Plotting.R
@@ -1,11 +1,49 @@
 library(ggplot2)
+library(dplyr)
 
 defaulttheme <- theme(axis.title = element_text(size = 11, color = "gray10"),
-                      axis.text = element_text(size = 8, color ="gray10"),
+                      axis.text = element_text(size = 9, color ="gray10"),
                       axis.ticks = element_line(color = "gray10"),
                       panel.background = element_blank(),
                       panel.border = element_rect(fill = NA, color = "gray10"),
                       legend.key = element_blank(), 
                       legend.position = "top",
+                      legend.box = "vertical",
+                      legend.box.just = "center",
+                      legend.box.spacing = unit(0.1,"cm"),
                       legend.title = element_text(size = 10, color = "gray10"),
                       legend.text = element_text(size = 9, color = "gray10"))
+
+#Plot observations as points
+series_plot <- function(
+    data,
+    x,
+    y,
+    group
+){
+  ggplot(data = data,
+         aes(x = {{x}}, y = {{y}}, group = {{group}}, color = {{group}})) + 
+    geom_point() +
+    scale_y_continuous(limits = c(min(c(0, data %>% pull({{y}}))), #plot 0
+                                  max(data %>% pull({{y}})))) +
+    defaulttheme
+}
+
+#Plot observations as points and model predictions as lines
+predictions_plot <- function(
+    data,
+    x,
+    y,
+    group,
+    predictions
+){
+  ggplot(data = data,
+         aes(x = {{x}}, y = {{y}}, group = {{group}}, color = {{group}})) + 
+    geom_point() +
+    geom_line(data = predictions) +
+    scale_y_continuous(limits = c(min(c(0, data %>% pull({{y}}),
+                                        predictions %>% pull({{y}}))), #plot 0
+                                  max(data %>% pull({{y}}),
+                                      predictions %>% pull({{y}})))) +
+    defaulttheme
+}
