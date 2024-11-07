0ab9c80e237714713b95a741bc67f4626472016f
Jack Buckner <Jhbuckner@ucdavis.edu>
2024-11-07 15:42:14 -0800

    added custom_derivatives_jl which constructs a cunstom derivatives model from a .jl file

--- src/model_constructors.R
+++ src/model_constructors.R
@@ -22,3 +22,34 @@ NODE <- function(
                    ",extrap_rho=",extrap_rho,")",
                    sep=""))
 }
+
+
+custom_derivatives_jl <- function(
+  data,
+  file,
+  time_column_name = "time",
+  hidden_units = 10,
+  seed = 1,
+  proc_weight = 1.0,
+  obs_weight = 1.0,
+  reg_weight = 10 ^ -6,
+  reg_type = "L2",
+  l = 0.25,
+  extrap_rho = 0.0
+){
+
+  julia_eval(paste0("include(\"", file, "\")"))
+  julia_assign("data_julia", data)
+  julia_eval(paste(
+                   "CustomDerivatives(data_julia,derivs,parameters,time_column_name=\"",
+                   time_column_name,
+                   "\",hidden_units=",hidden_units,
+                   ",seed=",seed,
+                   ",proc_weight=",proc_weight,
+                   ",obs_weight=",obs_weight,
+                   ",reg_weight=",reg_weight,
+                   ",reg_type=\"",reg_type,
+                   "\",l=",l,
+                   ",extrap_rho=", extrap_rho, ")",
+                   sep = ""))
+}
