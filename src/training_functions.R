28d5774779ba8def42568098f5980b163f4db041
Jorge Arroyo Esquivel <jorgearroyo0731@gmail.com>
2025-02-04 12:58:12 -0800

    Added basic functionality of train!

---
+++ src/training_functions.R
@@ -0,0 +1,13 @@
+train <- function(julia_model,
+                  loss_function = "derivative matching",
+                  optimizer = "ADAM",
+                  regularization_weight = 0.0,
+                  verbose = TRUE){
+  verbose <- ifelse(verbose,"true","false")
+  julia_assign("model",julia_model)
+  julia_eval(paste0("train!(model,loss_function",loss_function,
+                    ",optimizer=",optimizer,
+                    "regularization_weight",regularization_weight,
+                    "verbose",verbose,")"))
+  return(julia_eval("model",need_return = "Julia"))
+}
