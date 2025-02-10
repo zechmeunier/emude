15934d092bc6de36a4dfaea33ede3e0973d8d29f
Jorge Arroyo Esquivel <jorgearroyo0731@gmail.com>
2025-02-10 13:53:25 -0800

    Changed order of NamedTuple

--- src/training_functions.R
+++ src/training_functions.R
@@ -7,13 +7,13 @@ train <- function(julia_model,
                   optim_options = list()){
   verbose <- ifelse(verbose,"true","false")
   julia_assign("model",julia_model)
-  julia_assign("loss_options",NamedTuple(loss_options))
-  julia_assign("optim_options",NamedTuple(optim_options))
+  julia_assign("loss_options",loss_options)
+  julia_assign("optim_options",optim_options)
   julia_eval(paste0("train!(model,loss_function=",loss_function,
                     ",optimizer=",optimizer,
                     ",regularization_weight=",regularization_weight,
                     ",verbose=",verbose,
-                    "loss_options=loss_options",
-                    "optim_options=optim_options)"))
+                    "loss_options=NamedTuple(loss_options),",
+                    "optim_options=NamedTuple(optim_options))"))
   return(julia_eval("model",need_return = "Julia"))
 }
