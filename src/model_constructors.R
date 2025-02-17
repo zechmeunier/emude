f25a197785bcc0b712a0860b39bb62c2b13fb583
Zechariah Meunier <72064608+zechmeunier@users.noreply.github.com>
2025-02-17 12:09:27 -0800

    Add and test error messages

--- src/model_constructors.R
+++ src/model_constructors.R
@@ -1,57 +1,61 @@
 source("src/helpers.R")
 julia_eval("include(\"src/helpers.jl\")")
 NODE <- function(
-  data,
-  covariates = NULL,
-  time_column_name = "time",
-  hidden_units = 10,
-  seed = 1,
-  proc_weight = 1.0,
-  obs_weight = 1.0,
-  reg_weight = 10 ^ -6,
-  reg_type = "L2",
-  l = 0.25,
-  extrap_rho = 0.0,
-  bayesian = FALSE
+    data,
+    covariates = NULL,
+    time_column_name = "time",
+    hidden_units = 10,
+    seed = 1,
+    proc_weight = 1.0,
+    obs_weight = 1.0,
+    reg_weight = 10 ^ -6,
+    reg_type = "L2",
+    l = 0.25,
+    extrap_rho = 0.0,
+    bayesian = FALSE
 ){
+  if (max(data[, setdiff(names(data), time_column_name)], na.rm = TRUE) > 1) {
+    cat("Model performance may be improved by scaling the data through transformation or relativization.",
+        "Package options include relativization by column maximum (rel_colmax) and min-max normalization (rel_minmax).")
+  }
   model_type <- ifelse(bayesian,"BayesianNODE","NODE")
   julia_assign("data_julia",data)
   if(is.null(covariates)){
     julia_model <- julia_eval(paste0(model_type,
-                     "(data_julia,time_column_name=\"",time_column_name,
-                     "\",hidden_units=",hidden_units,
-                     ",seed=",seed,
-                     ",proc_weight=",proc_weight,
-                     ",obs_weight=",obs_weight,
-                     ",reg_weight=",reg_weight,
-                     ",reg_type=\"",reg_type,
-                     "\",l=",l,
-                     ",extrap_rho=",extrap_rho,")"),
-                    need_return = "Julia")
-
+                                     "(data_julia,time_column_name=\"",time_column_name,
+                                     "\",hidden_units=",hidden_units,
+                                     ",seed=",seed,
+                                     ",proc_weight=",proc_weight,
+                                     ",obs_weight=",obs_weight,
+                                     ",reg_weight=",reg_weight,
+                                     ",reg_type=\"",reg_type,
+                                     "\",l=",l,
+                                     ",extrap_rho=",extrap_rho,")"),
+                              need_return = "Julia")
+    
     julia_assign("julia_model_jl", julia_model)
-
+    
   }else{
     julia_assign("covariates_julia",covariates)
     julia_model <- julia_eval(paste(model_type,
-                     "(data_julia,covariates_julia,time_column_name=\"",time_column_name,
-                     "\",hidden_units=",hidden_units,
-                     ",seed=",seed,
-                     ",proc_weight=",proc_weight,
-                     ",obs_weight=",obs_weight,
-                     ",reg_weight=",reg_weight,
-                     ",reg_type=\"",reg_type,
-                     "\",l=",l,
-                     ",extrap_rho=",extrap_rho,")"),
-                    need_return = "Julia")
+                                    "(data_julia,covariates_julia,time_column_name=\"",time_column_name,
+                                    "\",hidden_units=",hidden_units,
+                                    ",seed=",seed,
+                                    ",proc_weight=",proc_weight,
+                                    ",obs_weight=",obs_weight,
+                                    ",reg_weight=",reg_weight,
+                                    ",reg_type=\"",reg_type,
+                                    "\",l=",l,
+                                    ",extrap_rho=",extrap_rho,")"),
+                              need_return = "Julia")
   }
-
+  
   # # train the model
   # julia_eval("train!(julia_model) # have a return conditon based on convergence")
-
+  
   # # function to convert julia_model to something R can return 
   # R_model <- julia_eval("return_for_R(julia_model)")
-
+  
   return(julia_model)
 }
 
@@ -73,6 +77,10 @@ custom_derivatives <- function(
     extrap_rho = 0.0,
     bayesian = FALSE
 ){
+  if (max(data[, setdiff(names(data), time_column_name)], na.rm = TRUE) > 1) {
+    cat("Model performance may be improved by scaling the data through transformation or relativization.",
+        "Package options include relativization by column maximum (rel_colmax) and min-max normalization (rel_minmax).")
+  }
   model_type <- ifelse(bayesian,"BayesianUDE","CustomDerivatives")
   
   if(is.character(derivs)) {
@@ -83,7 +91,7 @@ custom_derivatives <- function(
     translated_function <- R_to_Julia(derivs)
     julia_eval(paste("f_julia = ", translated_function))
   }
-
+  
   
   julia_assign("p_julia",initial_parameters)
   julia_eval("p_julia = NamedTuple(p_julia)", need_return = "Julia")
@@ -97,27 +105,27 @@ custom_derivatives <- function(
   
   if(is.null(covariates)){
     julia_model <- julia_eval(paste0(model_type,
-                     "(data_julia,deriv,parameters,time_column_name=\"",time_column_name,
-                     "\"",
-                     ",proc_weight=",proc_weight,
-                     ",obs_weight=",obs_weight,
-                     ",reg_weight=",reg_weight,
-                     ",reg_type=\"",reg_type,
-                     "\",l=",l,
-                     ",extrap_rho=",extrap_rho,")"),
-                    need_return = "Julia")
+                                     "(data_julia,deriv,parameters,time_column_name=\"",time_column_name,
+                                     "\"",
+                                     ",proc_weight=",proc_weight,
+                                     ",obs_weight=",obs_weight,
+                                     ",reg_weight=",reg_weight,
+                                     ",reg_type=\"",reg_type,
+                                     "\",l=",l,
+                                     ",extrap_rho=",extrap_rho,")"),
+                              need_return = "Julia")
   }else{
     julia_assign("covariates_julia",covariates)
     julia_model <- julia_eval(paste0(model_type,
-                     "(data_julia,covariates_julia,deriv,parameters,time_column_name=\"",time_column_name,
-                     "\"",
-                     ",proc_weight=",proc_weight,
-                     ",obs_weight=",obs_weight,
-                     ",reg_weight=",reg_weight,
-                     ",reg_type=\"",reg_type,
-                     "\",l=",l,
-                     ",extrap_rho=",extrap_rho,")"),
-                    need_return = "Julia")
+                                     "(data_julia,covariates_julia,deriv,parameters,time_column_name=\"",time_column_name,
+                                     "\"",
+                                     ",proc_weight=",proc_weight,
+                                     ",obs_weight=",obs_weight,
+                                     ",reg_weight=",reg_weight,
+                                     ",reg_type=\"",reg_type,
+                                     "\",l=",l,
+                                     ",extrap_rho=",extrap_rho,")"),
+                              need_return = "Julia")
   }
   return(julia_model)
 }
@@ -136,6 +144,10 @@ ode_model <- function(
     extrap_rho = 0.0,
     bayesian = FALSE
 ){
+  if (max(data[, setdiff(names(data), time_column_name)], na.rm = TRUE) > 1) {
+    cat("Model performance may be improved by scaling the data through transformation or relativization.",
+        "Package options include relativization by column maximum (rel_colmax) and min-max normalization (rel_minmax).")
+  }
   model_type <- ifelse(bayesian,"BayesianUDE","CustomDerivatives")
   translated_function <- R_to_Julia(derivs)
   julia_eval(paste("f_julia = ", translated_function))
@@ -151,29 +163,29 @@ ode_model <- function(
   
   if(is.null(covariates)){
     julia_model <- julia_eval(paste0(model_type,
-                     "(data_julia,deriv,parameters,time_column_name=\"",time_column_name,
-                     "\",hidden_units=",hidden_units,
-                     ",seed=",seed,
-                     ",proc_weight=",proc_weight,
-                     ",obs_weight=",obs_weight,
-                     ",reg_weight=",reg_weight,
-                     ",reg_type=\"",reg_type,
-                     "\",l=",l,
-                     ",extrap_rho=",extrap_rho,")"),
-                    need_return = "Julia")
+                                     "(data_julia,deriv,parameters,time_column_name=\"",time_column_name,
+                                     "\",hidden_units=",hidden_units,
+                                     ",seed=",seed,
+                                     ",proc_weight=",proc_weight,
+                                     ",obs_weight=",obs_weight,
+                                     ",reg_weight=",reg_weight,
+                                     ",reg_type=\"",reg_type,
+                                     "\",l=",l,
+                                     ",extrap_rho=",extrap_rho,")"),
+                              need_return = "Julia")
   }else{
     julia_assign("covariates_julia",covariates)
     julia_model <- julia_eval(paste0(model_type,
-                     "(data_julia,covariates_julia,deriv,parameters,time_column_name=\"",time_column_name,
-                     "\",hidden_units=",hidden_units,
-                     ",seed=",seed,
-                     ",proc_weight=",proc_weight,
-                     ",obs_weight=",obs_weight,
-                     ",reg_weight=",reg_weight,
-                     ",reg_type=\"",reg_type,
-                     "\",l=",l,
-                     ",extrap_rho=",extrap_rho,")"),
-                    need_return = "Julia")
+                                     "(data_julia,covariates_julia,deriv,parameters,time_column_name=\"",time_column_name,
+                                     "\",hidden_units=",hidden_units,
+                                     ",seed=",seed,
+                                     ",proc_weight=",proc_weight,
+                                     ",obs_weight=",obs_weight,
+                                     ",reg_weight=",reg_weight,
+                                     ",reg_type=\"",reg_type,
+                                     "\",l=",l,
+                                     ",extrap_rho=",extrap_rho,")"),
+                              need_return = "Julia")
   }
   return(julia_model)
 }
