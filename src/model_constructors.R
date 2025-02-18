6e4ef0c839a08e013ae9a5341798bc01167705a7
Zechariah Meunier <72064608+zechmeunier@users.noreply.github.com>
2025-02-18 09:41:14 -0800

    Add multi_custom_derivatives
    Delete seed from custom_derivatives as it's not used

--- src/model_constructors.R
+++ src/model_constructors.R
@@ -61,9 +61,8 @@ custom_derivatives <- function(
     covariates = NULL,
     neural_network_inputs = 1,
     neural_network_outputs = 1,
-    time_column_name = "time",
     hidden_units = 10,
-    seed = 1,
+    time_column_name = "time",
     proc_weight = 1.0,
     obs_weight = 1.0,
     reg_weight = 10^-6,
@@ -112,6 +111,77 @@ custom_derivatives <- function(
     julia_assign("covariates_julia",covariates)
     julia_model <- julia_eval(paste0(model_type,
                                      "(data_julia,covariates_julia,deriv,parameters,time_column_name=\"",time_column_name,"\"",
+                                     ",proc_weight=",proc_weight,
+                                     ",obs_weight=",obs_weight,
+                                     ",reg_weight=",reg_weight,
+                                     ",reg_type=\"",reg_type,"\"",
+                                     ",l=",l,
+                                     ",extrap_rho=",extrap_rho,")"),
+                              need_return = "Julia")
+  }
+  return(julia_model)
+}
+
+multi_custom_derivatives <- function(
+    data,
+    derivs,
+    initial_parameters,
+    covariates = NULL,
+    neural_network_inputs = 1,
+    neural_network_outputs = 1,
+    hidden_units = 10,
+    time_column_name = "time",
+    series_column_name = "series",
+    proc_weight = 1.0,
+    obs_weight = 1.0,
+    reg_weight = 10^-6,
+    reg_type = "L2",
+    l = 0.25,
+    extrap_rho = 0.0,
+    bayesian = FALSE
+){
+  if (sd(as.matrix(data[, setdiff(names(data), time_column_name)]), na.rm = TRUE) > 1) {
+    cat("Model performance may be improved by scaling the data through transformation or relativization.",
+        "Package options include relativization by column maximum (rel_colmax) and min-max normalization (rel_minmax).")
+  }
+  model_type <- ifelse(bayesian,"BayesianUDE","CustomDerivatives")
+  
+  if(is.character(derivs)) {
+    julia_eval(paste0('include("', derivs, '")'))
+    julia_eval("f_julia = derivs")
+  }
+  else{
+    translated_function <- R_to_Julia(derivs)
+    julia_eval(paste("f_julia = ", translated_function))
+  }
+  
+  
+  julia_assign("p_julia",initial_parameters)
+  julia_eval("p_julia = NamedTuple(p_julia)", need_return = "Julia")
+  
+  julia_assign("data_julia",data)
+  julia_assign("inputs",neural_network_inputs)
+  julia_assign("outputs",neural_network_outputs)
+  julia_assign("hidden_units",hidden_units)
+  
+  julia_model <- julia_eval("deriv, parameters = build_custom_derivs_function_R(f_julia,p_julia,inputs,hidden_units,outputs)")
+  
+  if(is.null(covariates)){
+    julia_model <- julia_eval(paste0(model_type,
+                                     "(data_julia,deriv,parameters,time_column_name=\"",time_column_name,"\"",
+                                     ",series_column_name=\"",series_column_name,"\"",
+                                     ",proc_weight=",proc_weight,
+                                     ",obs_weight=",obs_weight,
+                                     ",reg_weight=",reg_weight,
+                                     ",reg_type=\"",reg_type,"\"",
+                                     ",l=",l,
+                                     ",extrap_rho=",extrap_rho,")"),
+                              need_return = "Julia")
+  }else{
+    julia_assign("covariates_julia",covariates)
+    julia_model <- julia_eval(paste0(model_type,
+                                     "(data_julia,covariates_julia,deriv,parameters,time_column_name=\"",time_column_name,"\"",
+                                     ",series_column_name=\"",series_column_name,"\"",
                                      ",proc_weight=",proc_weight,
                                      ",obs_weight=",obs_weight,
                                      ",reg_weight=",reg_weight,
