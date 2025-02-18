0e18cea562eb270338e376c510a280449f30df6a
Zechariah Meunier <72064608+zechmeunier@users.noreply.github.com>
2025-02-18 10:46:33 -0800

    Add and test multi_NODE
    Works successfully, so the issue with multi_custom_derivatives is probably with the derivs file

--- src/model_constructors.R
+++ src/model_constructors.R
@@ -1,5 +1,6 @@
 source("src/helpers.R")
 julia_eval("include(\"src/helpers.jl\")")
+
 NODE <- function(
     data,
     covariates = NULL,
@@ -53,6 +54,61 @@ NODE <- function(
   return(julia_model)
 }
 
+multi_NODE <- function(
+    data,
+    covariates = NULL,
+    time_column_name = "time",
+    series_column_name = "series",
+    hidden_units = 10,
+    seed = 1,
+    proc_weight = 1.0,
+    obs_weight = 1.0,
+    reg_weight = 10^-6,
+    reg_type = "L2",
+    l = 0.25,
+    extrap_rho = 0.0,
+    bayesian = FALSE
+){
+  if (sd(as.matrix(data[, setdiff(names(data), c(time_column_name,series_column_name))]), na.rm = TRUE) > 1) {
+    cat("Model performance may be improved by scaling the data through transformation or relativization.",
+        "Package options include relativization by column maximum (rel_colmax) and min-max normalization (rel_minmax).")
+  }
+  model_type <- ifelse(bayesian,"BayesianNODE","MultiNODE")
+  julia_assign("data_julia",data)
+  if(is.null(covariates)){
+    julia_model <- julia_eval(paste0(model_type,
+                                     "(data_julia,time_column_name=\"",time_column_name,"\"",
+                                     ",series_column_name=\"",series_column_name,"\"",
+                                     ",hidden_units=",hidden_units,
+                                     ",seed=",seed,
+                                     ",proc_weight=",proc_weight,
+                                     ",obs_weight=",obs_weight,
+                                     ",reg_weight=",reg_weight,
+                                     ",reg_type=\"",reg_type,"\"",
+                                     ",l=",l,
+                                     ",extrap_rho=",extrap_rho,")"),
+                              need_return = "Julia")
+    
+    julia_assign("julia_model_jl", julia_model)
+    
+  }else{
+    julia_assign("covariates_julia",covariates)
+    julia_model <- julia_eval(paste(model_type,
+                                    "(data_julia,covariates_julia,time_column_name=\"",time_column_name,"\"",
+                                    ",series_column_name=\"",series_column_name,"\"",
+                                    ",hidden_units=",hidden_units,
+                                    ",seed=",seed,
+                                    ",proc_weight=",proc_weight,
+                                    ",obs_weight=",obs_weight,
+                                    ",reg_weight=",reg_weight,
+                                    ",reg_type=\"",reg_type,"\"",
+                                    ",l=",l,
+                                    ",extrap_rho=",extrap_rho,")"),
+                              need_return = "Julia")
+  }
+  
+  return(julia_model)
+}
 
 custom_derivatives <- function(
     data,
@@ -169,14 +225,14 @@ multi_custom_derivatives <- function(
   if(is.null(covariates)){
     
     print(paste0(model_type,
-                  "(data_julia,deriv,parameters,time_column_name=\"",time_column_name,"\"",
-                  ",series_column_name=\"",series_column_name,"\"",
-                  ",proc_weight=",proc_weight,
-                  ",obs_weight=",obs_weight,
-                  ",reg_weight=",reg_weight,
-                  ",reg_type=\"",reg_type,"\"",
-                  ",l=",l,
-                  ",extrap_rho=",extrap_rho,")"))
+                 "(data_julia,deriv,parameters,time_column_name=\"",time_column_name,"\"",
+                 ",series_column_name=\"",series_column_name,"\"",
+                 ",proc_weight=",proc_weight,
+                 ",obs_weight=",obs_weight,
+                 ",reg_weight=",reg_weight,
+                 ",reg_type=\"",reg_type,"\"",
+                 ",l=",l,
+                 ",extrap_rho=",extrap_rho,")"))
     
     julia_model <- julia_eval(paste0(model_type,
                                      "(data_julia,deriv,parameters,time_column_name=\"",time_column_name,"\"",
