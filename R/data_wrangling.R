4359333a94ae8da68aa1c9e0cecef218ddf282ba
Zechariah Meunier <72064608+zechmeunier@users.noreply.github.com>
2025-08-01 11:17:26 -1000

    Convert matrix to data frame

--- R/data_wrangling.R
+++ R/data_wrangling.R
@@ -2,45 +2,47 @@
 #'
 #' `colmax()` calculates the maximum (highest) value per column.
 #'
-#' @param mat A matrix with any dimensions.
+#' @param df A data frame with any dimensions.
 #'
 #' @return Vector of column maxima, with length equal to the number of columns.
 #'
 #' @examples
-#' X <- matrix(data = seq(1,100), nrow = 20, ncol = 5)
-#' colmax(mat = X)
+#' X <- data.frame("speciesA" = rpois(40,2),
+#'                 "speciesB" = rpois(40,3))
+#' colmax(df = X)
 #'
 #' @export
-colmax <- function(mat) {
-  colmaxes <- as.numeric(sapply(mat, function(x) ifelse(all(is.na(x)), NA, max(x, na.rm = TRUE))))
+colmax <- function(df) {
+  colmaxes <- as.numeric(sapply(df, function(x) ifelse(all(is.na(x)), NA, max(x, na.rm = TRUE))))
   colmaxes  }
 
 #' Calculate the column minima
 #'
 #' `colmin()` calculates the minimum (lowest) value per column.
 #'
-#' @param mat A matrix with any dimensions.
+#' @param df A data frame with any dimensions.
 #'
 #' @return Vector of column minima, with length equal to the number of columns.
 #'
 #' @examples
-#' X <- matrix(data = seq(1,100), nrow = 20, ncol = 5)
-#' colmax(mat = X)
+#' X <- data.frame("speciesA" = rpois(40,2),
+#'                 "speciesB" = rpois(40,3))
+#' colmax(df = X)
 #'
 #' @export
-colmin <- function(mat) {
-  colmins <- as.numeric(sapply(mat, function(x) ifelse(all(is.na(x)), NA, min(x, na.rm = TRUE))))
+colmin <- function(df) {
+  colmins <- as.numeric(sapply(df, function(x) ifelse(all(is.na(x)), NA, min(x, na.rm = TRUE))))
   colmins  }
 
-#' Relativize matrix by its column maxima
+#' Relativize data frame by its column maxima
 #'
-#' `rel_colmax()` relativizes a matrix by the maximum values per column. It will exclude the specified time and series columns.
+#' `rel_colmax()` relativizes a data frame by the maximum values per column. It will exclude the specified time and series columns.
 #'
 #' @param time_column_name The column that contains the time data, indicating when the observations were made.
 #' @param series_column_name The column that contains the series data, indicating the identifying information for the observations.
-#' @param mat A matrix with any dimensions containing `time_column_name`, `series_column_name`, and observations.
+#' @param df A data frame with any dimensions containing `time_column_name`, `series_column_name`, and observations.
 #'
-#' @return A matrix scaled so that values are proportions of the maximum value per column.
+#' @return A data frame scaled so that values are proportions of the maximum value per column.
 #'
 #' @examples
 #' X <- data.frame("time" = seq(1,40),
@@ -50,28 +52,28 @@ colmin <- function(mat) {
 #' rel_colmax(X, time_column_name = "time", series_column_name = "transect")
 #'
 #' @export
-rel_colmax <- function(mat, time_column_name = "time", series_column_name = "series") {
+rel_colmax <- function(df, time_column_name = "time", series_column_name = "series") {
 
-  cols_to_exclude <- intersect(c(time_column_name, series_column_name), names(mat))
-  cols_to_rel <- setdiff(names(mat), cols_to_exclude)
-  mat_to_rel <- mat[cols_to_rel]
+  cols_to_exclude <- intersect(c(time_column_name, series_column_name), names(df))
+  cols_to_rel <- setdiff(names(df), cols_to_exclude)
+  df_to_rel <- df[cols_to_rel]
 
-  colmaxes <- colmax(mat_to_rel)
+  colmaxes <- colmax(df_to_rel)
 
-  mat[cols_to_rel] <- sweep(mat_to_rel, 2, colmaxes, `/`)
+  df[cols_to_rel] <- sweep(df_to_rel, 2, colmaxes, `/`)
 
-  return(mat)
+  return(df)
 }
 
-#' Relativize matrix by its column maxima and minima
+#' Relativize data frame by its column maxima and minima
 #'
-#' `rel_minmax()` relativizes a matrix by the maximum and minimum values per column. It will exclude the specified time and series columns.
+#' `rel_minmax()` relativizes a data frame by the maximum and minimum values per column. It will exclude the specified time and series columns.
 #'
 #' @param time_column_name The column that contains the time data, indicating when the observations were made.
 #' @param series_column_name The column that contains the series data, indicating the identifying information for the observations.
-#' @param mat A matrix with any dimensions containing `time_column_name`, `series_column_name`, and observations.
+#' @param df A data frame with any dimensions containing `time_column_name`, `series_column_name`, and observations.
 #'
-#' @return A matrix scaled so that minimum and maximum values per column are 0 and 1, respectively.
+#' @return A data frame scaled so that minimum and maximum values per column are 0 and 1, respectively.
 #'
 #' @examples
 #' X <- data.frame("time" = seq(1,40),
@@ -81,19 +83,19 @@ rel_colmax <- function(mat, time_column_name = "time", series_column_name = "ser
 #' rel_colmax(X, time_column_name = "time", series_column_name = "transect")
 #'
 #' @export
-rel_minmax <- function(mat, time_column_name = "time", series_column_name = "series") {
+rel_minmax <- function(df, time_column_name = "time", series_column_name = "series") {
 
-  cols_to_exclude <- intersect(c(time_column_name, series_column_name), names(mat))
-  cols_to_rel <- setdiff(names(mat), cols_to_exclude)
-  mat_to_rel <- mat[cols_to_rel]
+  cols_to_exclude <- intersect(c(time_column_name, series_column_name), names(df))
+  cols_to_rel <- setdiff(names(df), cols_to_exclude)
+  df_to_rel <- df[cols_to_rel]
 
-  colmins <- colmin(mat_to_rel)
-  colmaxes <- colmax(mat_to_rel)
+  colmins <- colmin(df_to_rel)
+  colmaxes <- colmax(df_to_rel)
 
-  mat[cols_to_rel] <- sweep(
-    sweep(mat_to_rel, 2, colmins, `-`),
+  df[cols_to_rel] <- sweep(
+    sweep(df_to_rel, 2, colmins, `-`),
     2, colmaxes - colmins, `/`
   )
 
-  return(mat)
+  return(df)
 }
