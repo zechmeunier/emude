f776e3b50004ba3cd36640a568bcc4040747c829
Zechariah Meunier <72064608+zechmeunier@users.noreply.github.com>
2025-08-04 09:27:03 -0700

    Write documentation

--- R/load_save_models.R
+++ R/load_save_models.R
@@ -1,16 +1,31 @@
-#' Load Julia Model Weights
+#' Load the Julia model weights
 #'
-#' Description here
+#' Loads the weights and other parameters of the process model from Julia
 #'
-#' @param model description
-#' @param file description
-#' @return description
+#' @param model User-defined model containing the weights, biases, and other parameters.
+#' @param file File path from which to read the model parameters. Files are saved in JSON format.
+#'
+#' @return A data frame containing the parameters from the trained model.
 #' @export
+#'
+#' @examples
+#' print(x)
+#'
 load_julia_model_weights <- function(model,file){
   JuliaCall::julia_eval(paste0("load_model_parameters!(",model,",",file,")"))
 }
 
+#' Save the Julia model weights
+#'
+#' @param model User-defined model containing the weights, biases, and other parameters.
+#' @param file File path to which to write the model parameters. Files are saved in JSON format.
+#'
+#' @return A file containing the parameters from the trained model.
 #' @export
+#'
+#' @examples
+#' print(x)
+#'
 save_julia_model_weights <- function(model,file){
   JuliaCall::julia_eval(paste0("save_model_parameters(",model,",",file,")"))
 }
--- R/model_analysis.R
+++ R/model_analysis.R
@@ -1,17 +1,34 @@
-#' Cross Validation
+#' Leave-future-out cross-validation
 #'
-#' description
+#' `cross_validation()` runs leave-future-out cross-validation on the UDE model
+#' using a training routine with **k**-folds. The function returns three data
+#' frames. The first contains an estimate of the mean absolute error of the
+#' forecasts and associated standard error as a function of the forecast horizon
+#' (1 to **k** time steps into the future). The second and third are returned in a
+#' named tuple with two elements `horizon_by_var` and `raw`. The data frame
+#' `horizon_by_var` contains the forecasting errors separated by variable
+#' and the data frame `raw` contains the raw testing and forecasting data. If
+#' the model is trained on multiple time series, the named tuple will include a
+#' third data frame `horizon_by_var_by_series`.
 #'
-#' @param model description
-#' @param k description
-#' @param loss_function description
-#' @param optimizer description
-#' @param regularization_weight description
-#' @param verbose description
-#' @param loss_options description
-#' @param optim_options description
-#' @param path description
+#' @param model The UDE model to be tested with cross-validation.
+#' @param k The number of folds for cross-validation. The default is 10 and the
+#' recommended minimum number is 3.
+#' @param loss_function Missing description in Julia
+#' @param optimizer Missing description in Julia
+#' @param regularization_weight Missing description in Julia
+#' @param verbose Missing description in Julia
+#' @param loss_options Missing description in Julia
+#' @param optim_options Missing description in Julia
+#' @param path File path for saving .csv files containing the raw testing data
+#' and forecasts for each fold.
+#'
+#' @return Several data frames.
 #' @export
+#'
+#' @examples
+#' print(x)
+#'
 cross_validation <- function(
     model,
     k = 10,
