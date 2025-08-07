6a8a0c1db8ee0bcdec007d87a83200029b7f3605
Zechariah Meunier <72064608+zechmeunier@users.noreply.github.com>
2025-08-07 16:44:09 -0500

    Write help text and examples for plotting functions

--- R/plotting.R
+++ R/plotting.R
@@ -1,4 +1,3 @@
-#' @importFrom ggplot2 theme
 defaulttheme <- theme(axis.title = element_text(size = 11, color = "black"),
                       axis.text = element_text(size = 9, color ="black"),
                       axis.ticks = element_line(color = "black"),
@@ -11,17 +10,33 @@ defaulttheme <- theme(axis.title = element_text(size = 11, color = "black"),
                       legend.box.spacing = unit(0.1,"cm"),
                       legend.title = element_text(size = 11, color = "black"),
                       legend.text = element_text(size = 9, color = "black"))
-#' Series Plot
+
+#' Plot time series
 #'
-#' Plot observations as points and model predictions as lines in a time series
+#' `series_plot()` creates a time series figure with observations as points and
+#' model predictions as lines.
+#'
 #' @import ggplot2
-#' @param observations DataFrame of observed data to use to create the plot
-#' @param x description
-#' @param y description
-#' @param group description
-#' @param predictions description
-#' @return description
+#'
+#' @param observations A data frame of observed values over time.
+#' @param x The column containing the values for the time variable (e.g., days).
+#' @param y The column containing the values for the response variable (e.g.,
+#' species abundances).
+#' @param group The column containing the identifier for the response variable
+#' (e.g., series or species name). Also defaults to the color scheme, but this
+#' can be adjusted with user-defined call to `aes(color = variable)`.
+#' @param predictions A data frame of predicted values over time, with column
+#' names matching the column names of `observations`. Optional.
+#'
+#' @return A `ggplot2` figure showing the time series of observations and model
+#' predictions. Can be adjusted with additional `ggplot2` functions.
 #' @export
+#'
+#' @examples
+#' df <- data.frame("time" = rep(seq(1,100),4),
+#'                  "species" = c(rep("A",100),rep("B",100),rep("C",100),rep("D",100)),
+#'                  "abundance" = rpois(400,40))
+#' series_plot(observations = df, x = "time", y = "abundance", group = "species")
 series_plot <- function(
     observations,
     x,
@@ -48,18 +63,35 @@ series_plot <- function(
   }
 }
 
-#' Phase Plane 2D
+
+#' Plot two-dimensional phase plane
 #'
-#' Plot observations as points and model predictions as lines in a 2D phase plane
-#' @param observations description
-#' @param names description
-#' @param values description
-#' @param x description
-#' @param y description
-#' @param predictions description
-#' @return description
-#' @export
+#' `phase_plane_2D()` creates a two-dimensional phase plane showing how the two
+#' state variables co-occur at each time point. Observations are plotted as points
+#' and model predictions as lines.
+#'
 #' @import ggplot2
+#'
+#' @param observations A data frame of observed values over time.
+#' @param names The column containing the identifier for the response variable
+#' (e.g., series or species name).
+#' @param values The column containing the values for the response variable (e.g.,
+#' species abundances).
+#' @param x The state variable to be displayed on the x-axis.
+#' @param y The state variable to be displayed on the y-axis.
+#' @param predictions A data frame of predicted values over time, with column
+#' names matching the column names of `observations`. Optional.
+#'
+#' @return A `ggplot2` figure showing the time series of observations and model
+#' predictions. Can be adjusted with additional `ggplot2` functions.
+#' @export
+#'
+#' @examples
+#' df <- data.frame("time" = rep(seq(1,100),4),
+#'                  "species" = c(rep("A",100),rep("B",100),rep("C",100),rep("D",100)),
+#'                  "abundance" = rpois(400,40))
+#' phase_plane_2D(observations = df, names = "species", values = "abundance",
+#'                x = "A", y = "B")
 phase_plane_2D <- function(
     observations,
     names,
